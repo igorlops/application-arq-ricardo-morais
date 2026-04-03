66918cea2f23cd40f815f6bedafd26d4c586a10c
Igor Lopes <claudioigorai@gmail.com>
2026-04-03 16:06:48 -0300

    fix: correção dos dominios

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://chale.arquitetoricardomoraisprojetos.com/', 'https://reforma.arquitetoricardomoraisprojetos.com/'],
+    'allowed_origins' => ['https://chale.arquitetoricardomoraisprojetos.com', 'https://reforma.arquitetoricardomoraisprojetos.com'],
 
     'allowed_origins_patterns' => [],
 
