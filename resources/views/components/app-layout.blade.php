cf3911f809b4a5ea2255794a8d1dde8a7e6ada47
Igor Lopes <cifernandes@sfiec.org.br>
2026-04-22 21:43:22 -0300

    meta tag verificação

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="google-site-verification" content="sjX2hSKq626VogbX3up1-XFzrcHjoXgeToIzsCibb_s" />
     @if(isset($seoMeta) && $seoMeta)
         <title>{{ $seoMeta->meta_title ?? config('app.name', 'Arquitetura') }}</title>
         @if($seoMeta->meta_description)
