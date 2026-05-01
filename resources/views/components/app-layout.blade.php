94141d551f7236d4221eb03c448e3f9baf7f2fb8
Igor Lopes <cifernandes@sfiec.org.br>
2026-05-01 15:09:23 -0300

    fix: dd para debugar

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="google-site-verification" content="sjX2hSKq626VogbX3up1-XFzrcHjoXgeToIzsCibb_s" />
+    @dd($seoMeta)
     @if(isset($seoMeta) && $seoMeta)
         <title>{{ $seoMeta->meta_title ?? config('app.name', 'Arquitetura') }}</title>
         @if($seoMeta->meta_description)
